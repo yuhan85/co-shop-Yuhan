8699bd3e567945fda5536254cf30afa87029eaa9
Jacky Gao <honglei.gao@hotmail.com>
2025-02-07 19:08:23 -0400

    update app.ts to include cart, cartitem, category, order, orderitem, payment, product, review, store routes

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,13 +1,32 @@
 import express, { Request, Response, NextFunction } from 'express';
-//import countryRoutes from './routes/countryRoutes';
+import countryRoutes from './routes/countryRoutes';
 import couponRoutes from './routes/couponRoutes';
+import orderRoutes from './routes/orderRoutes';
+import productRoutes from './routes/productRoutes';
+import reviewRoutes from './routes/reviewRoutes';
+import storeRoutes from './routes/storeRoutes';
+import cartRoutes from './routes/cartRoutes';
+import cartitemRoutes from './routes/cartitemRoutes';
+import orderitemRoutes from './routes/orderitemRoutes';
+import paymentRoutes from './routes/paymentRoutes';
+
+
 import { errorHandler } from './middleware/errorMiddleware';
 
 const app = express();
 
 app.use(express.json()); // Middleware to parse JSON
+
 app.use('/api/countries', countryRoutes); // Use country routes
 app.use('/api/coupons', couponRoutes);
+app.use('/api/orders', orderRoutes);
+app.use('/api/products', productRoutes);
+app.use('/api/reviews', reviewRoutes);
+app.use('/api/stores', storeRoutes);
+app.use('/api/carts', cartRoutes);
+app.use('/api/cartitems', cartitemRoutes);
+app.use('/api/orderitems', orderitemRoutes);
+app.use('/api/payments', paymentRoutes);
 
 // Explicitly cast errorHandler to an ErrorRequestHandler function
 app.use(errorHandler as express.ErrorRequestHandler);
